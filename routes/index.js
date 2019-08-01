de9bc72f148d874768a385af7b7c21e3aad175a6
Aroldo Bettega <aroldo.bettega@gmail.com>
2019-08-01 14:40:23 -0300

    xml2json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router  = express.Router();
+const axios = require('axios').create({});
+const parser = require('xml2js');
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  // res.render('index');
+router.get('/api/fill', (req, res, next) => {
+  let parseString = parser.parseString;
+  axios.get('http://legis.senado.leg.br/dadosabertos/senador/lista/atual')
+    .then(result => {
+      res.status(200).json(result.data.ListaParlamentarEmExercicio)
+    })
+    .catch(err => res.status(400).json(err));
 });
 
 module.exports = router;
