9cdcbb63010a29387e2dc44a48517c61073424e5
Alexandre M. Bonomi <alexandrembonomi@gmail.com>
2019-08-03 19:59:40 -0300

    O resultado nunca chega, porque algumas promises nunca resolvem. Preciso fazer ser apenas 1 promisse que chama alguma funcao recursiva

--- routes/deputados.js
+++ routes/deputados.js
@@ -7,13 +7,21 @@ const async = require('async');
 router.get('/propostas/:idAutor', (req, res, next) => {
   let {idAutor} = req.params;
   // di = dataInicial - YYYY-MM-DD, df = dataFinal - YYYY-MM-DD
-  let {di = '2000-01-01', df = ''} = req.query;
+  let {di = '2000-01-01', df} = req.query;
+  let url = `https://dadosabertos.camara.leg.br/api/v2/proposicoes?idDeputadoAutor=${idAutor}&dataApresentacaoInicio=${di}&itens=100&ordem=ASC&ordenarPor=id`;
 
-  let proposicoesArr = [];
+  if(df) {
+    url = `https://dadosabertos.camara.leg.br/api/v2/proposicoes?idDeputadoAutor=${idAutor}&dataApresentacaoInicio=${di}&dataApresentacaoFim=${df}&itens=100&ordem=ASC&ordenarPor=id`;
+  }
 
-  let url = `https://dadosabertos.camara.leg.br/api/v2/proposicoes?idDeputadoAutor=${idAutor}&dataApresentacaoInicio=${di}&dataApresentacaoFim=${df}&itens=100&ordem=ASC&ordenarPor=id`;
-
-  verifyProp(url)
+  let propostasArr = [];
+  
+  verifyProp(url, propostasArr)
+  .then(propostas => { console.log(propostas);
+    res.status(200).json(propostas.length)
+  })
+  .catch(err => res.status(400).json({err, message: 'Não conseguimos recuperar as propostas para esse deputado'}));
+  ;
 });
 
 // Pegar Proposta Específica
@@ -23,32 +31,40 @@ router.get('/proposta/:idProposta', (req, res, next) => {
   let {st, num, ano} = req.query;
 });
 
-const dateTreater = data => {
-  console.log('DATA PORRA', data)
-  if (data = '') {
-    return JSON.stringify(new Date()).split('T')[0];
-  }
-  console.log('SPLIT CARAIO', JSON.stringify(data).split('T')[0])
-  return JSON.stringify(data).split('T')[0];
-}
+// const dateTreater = data => {
+//   console.log('DATA PORRA', data)
+//   if (data = '') {
+//     return JSON.stringify(new Date()).split('T')[0];
+//   }
+//   console.log('SPLIT CARAIO', JSON.stringify(data).split('T')[0])
+//   return JSON.stringify(data).split('T')[0];
+// }
 
-const verifyProp = url => {
-  axios.get(url)
-  .then(res => {
-      let arrFunc = [];
+const verifyProp = (url, propostas) => { 
+  const promise = new Promise((resolve, reject) => {
+    axios.get(url)
+    .then(res => {
       res.data.dados.forEach(prop => {
-        arrFunc.push(() => {
-          let uri = prop.uri;
-          axios.get(uri)
-            .then(result => {
-              console.log(result.data)
-            })
-            .catch(e => console.log(e))
-        })
+        propostas.push(prop);
+       });
+
+       let next = false;
+       res.data.links.forEach(link => {
+         if (link.rel === 'next') {
+           next = true;
+           verifyProp(link.href, propostas);
+         }
+       });
+
+       //  console.log(next);
+       if (!next) {
+        //  console.log(propostas);
+         resolve(propostas);
+       }
       })
-      async.series(arrFunc, () => console.log('rodou carai'))
-    })
-    .catch(err => console.log('err'))
+      .catch(err => reject(err))
+})
+  return promise;
 }
 
 module.exports = router;
