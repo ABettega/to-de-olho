1990d4603ea4840252c894e929557cc75ffe5b87
Aroldo Bettega <aroldo.bettega@gmail.com>
2019-08-05 16:35:09 -0300

    email validation working

--- models/User.js
+++ models/User.js
@@ -10,6 +10,7 @@ const userSchema = new Schema({
   day: {type: Number, required: true},
   month: {type: String, required: true},
   year: {type: Number, required: true},
+  validated: {type: Boolean, default: false}
 });
 
 const User = mongoose.model("User", userSchema);
