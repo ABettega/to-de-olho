3f0161ed2527928e764fe83f3cfbae35721012d0
Alexandre M. Bonomi <alexandrembonomi@gmail.com>
2019-08-08 18:22:08 -0300

    Detalhes de votações completo

--- routes/deputados/deputados.js
+++ routes/deputados/deputados.js
@@ -10,13 +10,24 @@ router.get('/atuais', (req, res, next) => {
     .catch(e => console.log(e))  
 });
 
-// Pegar todas as propostas de um autor
+// Pegar um deputado específico
 router.get('/:idDeputado', (req, res, next) => {
   let {idDeputado} = req.params;
   
   Deputado.find({id: idDeputado})
     .then(dep => {
-      res.status(200).json(dep)
+      res.status(200).json(dep);
+    })
+    .catch(e => console.log(e))  
+});
+
+// Pegar um deputado específico por nome
+router.get('/nome/:nomeDeputado', (req, res, next) => {
+  let {nomeDeputado} = req.params;
+  
+  Deputado.find({nomeDeputado: nomeDeputado})
+    .then(dep => {
+      res.status(200).json(dep[0]);
     })
     .catch(e => console.log(e))  
 });
@@ -24,9 +35,19 @@ router.get('/:idDeputado', (req, res, next) => {
 router.get('/', (req, res, next) => {
   Deputado.find()
     .then(deps => {
-      res.status(200).json(deps)
+      res.status(200).json(deps);
     })
     .catch(e => console.log(e))  
+});
+
+// Pegar todos os deputados de uma legislatura 
+router.get('/legislatura/:idLegislatura', (req, res, next) => {
+  Deputado.find({idLegislatura: req.params.idLegislatura}, 
+    {_id: 0, id: 1, nomeDeputado: 1, siglaPartido: 1, siglaUf: 1, urlFoto: 1})
+  .then(deputados => {
+    res.status(200).json(deputados);
+  })
+  .catch(e => console.log(e));
 });
 
 module.exports = router;
--- routes/deputados/votacoes.js
+++ routes/deputados/votacoes.js
@@ -2,19 +2,23 @@ const express = require('express');
 const router  = express.Router();
 const SessaoCamara = require('../../models/SessaoCamara');
 
+const showDate = date => {
+  date = date.split('-');
+  return `${date[2]}/${date[1]}/${date[0]}`;
+}
+
 router.get('/:idVotacao', (req, res, next) => {
   SessaoCamara.find({"votacoes._id": req.params.idVotacao}, {listaDePresenca: 0, _id: 0}).
   then(sessao => {
-    // res.status(200).json(sessao);
     if(sessao.length > 0) {
       sessao[0].votacoes.forEach(votacao => {
         if (String(votacao._id) === req.params.idVotacao) {
           res.status(200).json({
-            document: votacao.documento, 
+            documento: votacao.documento, 
             proposicao: votacao.proposicao, 
             modo: votacao.modo,
             votos: votacao.votos,
-            data: sessao.dataInicio,
+            data: showDate(JSON.stringify(sessao[0].dataInicio).slice(1, 10)),
           });
         }
       });
@@ -23,7 +27,7 @@ router.get('/:idVotacao', (req, res, next) => {
       res.status(200).json({message: 'Não encontramos essa votação!'});
     }
   })
-  .catch(e => res.status(400).json({message: 'Erro no DB!'}));
+  .catch(e => res.status(400).json({message: `Ops! Tivemos um erro: ${e}`}));
 });
 
 module.exports = router;
